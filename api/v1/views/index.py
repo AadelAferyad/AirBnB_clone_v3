c805137ce68b924f159cdb27b9d70513374493af
AadelAferyad <animevineu@gmail.com>
2024-04-26 16:44:25 +0100

    api/states

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,14 @@
 """ for blueprint """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route("/status", strict_slashes=False, methods=['GET'])
 def status():
     return jsonify(status="OK")
+
+
+@app_views.route("/stats", strict_slashes=False, methods=['GET'])
+def stats():
+    return jsonify(storage.count())
